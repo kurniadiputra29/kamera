88e47c418f681bc8ea04275f74f624a92256df97
fias01 <mas.ipung2000@gmail.com>
2019-06-24 10:55:18 +0700

    update sidebar

--- resources/views/layouts/part/sidebar.blade.php
+++ resources/views/layouts/part/sidebar.blade.php
@@ -24,58 +24,76 @@
       </form>
       <!-- /.search form -->
       <!-- sidebar menu: : style can be found in sidebar.less -->
-      <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">MAIN NAVIGATION</li>
+      <aside class="main-sidebar">
+   <!-- sidebar: style can be found in sidebar.less -->
+   <section class="sidebar">
+     <!-- Sidebar user panel -->
+     <div class="user-panel">
+       <div class="pull-left image">
+         <img src="{{asset('AdminLTE/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
+       </div>
+       <div class="pull-left info">
+         <p>Alexander Pierce</p>
+         <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+       </div>
+     </div>
+     <!-- search form -->
+     <form action="#" method="get" class="sidebar-form">
+       <div class="input-group">
+         <input type="text" name="q" class="form-control" placeholder="Search...">
+         <span class="input-group-btn">
+               <button type="submit" name="search" id="search-btn" class="btn btn-flat">
+                 <i class="fa fa-search"></i>
+               </button>
+             </span>
+       </div>
+     </form>
+     <!-- /.search form -->
+     <!-- sidebar menu: : style can be found in sidebar.less -->
+     <ul class="sidebar-menu" data-widget="tree">
+       <li class="header">MAIN NAVIGATION</li>
+       <li class="{{ Request::is('admin/dashboard') || Request::is('admin/dashboard/*') ? 'active' : '' }}">
+         <a href="{{ url('/admin/dashboard') }}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
+       </li>
 
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-dashboard"></i>
-            <span>Dashboard</span>
-          </a>
-        </li>
+       <li class="treeview {{ Request::is('admin/categories') || Request::is('admin/categories/*') || Request::is('admin/products') || Request::is('admin/products/*') ? 'active' : '' }}">
+         <a href="#">
+           <i class="fa fa-home"></i> <span>Products</span>
+           <span class="pull-right-container">
+             <i class="fa fa-angle-left pull-right"></i>
+           </span>
+         </a>
+         <ul class="treeview-menu">
+           <li class="{{ Request::is('admin/categories') || Request::is('admin/categories/*') ? 'active' : '' }}">
+             <a href="{{ route('categories.index') }}"><i class="fa fa-circle-o"></i> Categories</a>
+           </li>
+           <li class="{{ Request::is('admin/products') || Request::is('admin/products/*') ? 'active' : '' }}">
+             <a href="#"><i class="fa fa-circle-o"></i> Items</a>
+           </li>
+         </ul>
+       </li>
 
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-folder-o"></i> <span>Inventory</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="{{ route('categories.index') }}"><i class="fa fa-circle-o"></i> Category</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Item</a></li>
-          </ul>
-        </li>
+       <li class="{{ Request::is('admin/rentals') || Request::is('admin/rentals/*') ? 'active' : '' }}">
+         <a href="#"><i class="fa fa-shopping-cart"></i> <span>Rental</span></a>
+       </li>
 
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-file-text-o"></i>
-            <span>Rental</span>
-          </a>
-        </li>
+       <li class="{{ Request::is('admin/returns') || Request::is('admin/returns/*') ? 'active' : '' }}">
+         <a href="#"><i class="fa fa-undo"></i> <span>Returns</span></a>
+       </li>
 
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-history"></i>
-            <span>Return</span>
-          </a>
-        </li>
+       <li class="{{ Request::is('admin/payments') || Request::is('admin/payments/*') ? 'active' : '' }}">
+         <a href="{{ route('payments.index') }}"><i class="fa fa-cc-visa"></i> <span>Payments</span></a>
+       </li>
 
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-cc-visa "></i>
-            <span>Payment</span>
-          </a>
-        </li>
+       <li class="{{ Request::is('admin/user') || Request::is('admin/user/*') ? 'active' : '' }}">
+         <a href="{{ route('user.index') }}"><i class="fa fa-users"></i> <span>Users</span></a>
+       </li>
 
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-user "></i>
-            <span>User</span>
-          </a>
-        </li>
+     </ul>
+   </section>
+   <!-- /.sidebar -->
+ </aside>
 
-      </ul>
     </section>
     <!-- /.sidebar -->
   </aside>
